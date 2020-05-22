6e1c49f14b2656b3e95e6880a736c6b9fb161911
강민재 <38933647+phoi5675@users.noreply.github.com>
2020-05-22 22:07:09 +0900

    Add files via upload
    smunotifier 와 동일하게 main 함수 추가 / 코드 가독성 강화
    취업 공지 추가

---
+++ const.py
@@ -0,0 +1,34 @@
+
+# 일반, 학사공지
+GENERAL = "http://kau.ac.kr/page/kauspace/general_list.jsp" # 일반
+ACADEMIC = "http://kau.ac.kr/page/kauspace/academicinfo_list.jsp" # 학사
+SCHOLAR = "http://www.kau.ac.kr/page/kauspace/scholarship_list.jsp" # 장학 / 대출
+RESEARCH = "http://www.kau.ac.kr/page/kauspace/research_list.jsp" # 산학 / 연구
+EVENT = "http://www.kau.ac.kr/page/kauspace/event_list.jsp" # 행사
+EMPLOY = "http://www.kau.ac.kr/page/kauspace/employment_list.jsp" # 모집 / 채용
+CAREER = "http://career.kau.ac.kr/ko/community" # 취업
+
+# 학과공지
+MACH = "http://www.kau.ac.kr/page/web/am_engineer/notice/dept_li.jsp"  # 항우기
+ELEC = "http://www.kau.ac.kr/page/dept/eie/board/undergraduate_notice.jsp"  # 항전정
+SOFT = "http://sw.kau.ac.kr/page_id=739"  # 소프트
+STUF = "http://www.kau.ac.kr/page/web/aviation_stuff/notice/dept_li.jsp"  # 재료
+LAWS = "http://www.kau.ac.kr/page/web/universe_law/life/notice_li.jsp"  # 교물
+AVIA = "http://www.kau.ac.kr/page/web/aviation_service/information/no_dept_li.jsp"  # 운항
+BUSI = "http://www.kau.ac.kr/page/web/business/community/news_li.jsp"  # 경영
+FREE = "http://www.kau.ac.kr/page/school/free/notice/notice_li.jsp"  # 자유
+
+
+# 학교 전체 공지 배열
+KAUARY = [GENERAL, ACADEMIC, SCHOLAR, RESEARCH, EVENT, EMPLOY, CAREER]
+
+# 학과 공지 배열 / 소프트 추가 X
+DEPARY = [MACH, ELEC, STUF, LAWS, AVIA, BUSI, FREE]
+
+# 파일 저장용 배열
+GENFIARY = ["gen", "aca", "sch", "res", "eve", "emp", "car"]
+DEPFIARY = ["mach", "elec", "stuf", "laws", "avia", "busi", "free"]
+
+# 파일 경로 / 루트 폴더까지만 지정
+#FILEPATH = "/home/phoi/Documents/web_scrap/"
+FILEPATH = "./"
